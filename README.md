963bd4e180b496590b024da387d3ab3694d472cc
ozzywithab <s10194236@connect.np.edu.sg>
2020-12-27 23:04:52 +0800

    Implemented API (all commented out)

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ID_S10194236_OzzyThamFooMing_Assg02
 ID Assignment 2
+
+# RatesAPI Website (Currency exchange rates) (ratesapi.io)
+
+## 2nd push, 27/12/2020 (EVERYTHING IN SCRIPT.JS IS COMMENTED OUT)
+
+### Things done
+* Did extremely basic Javascript and implemented RatesAPI
+
+* Comments added to make js file more readable and easy to understand
+
+### To-do/Features I want to add
+
+* Add basic html and css to make website usable
+
+* Allow user to compare exchange rates side by side (current and previous exchange rates)
+
+* Add a history tab for user to see past inputs/exchanges made
+
+* Possibly find another exchange rate API since RatesAPI is relatively simple/doesn't have many features
+
+* Refresh page regularly to show live, accurate exchange rates
+
+* Implement graphs to show currency history (Find another API to do this)
--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 
-fetch ("https://api.ratesapi.io/api/latest?base=USD&symbols=GBP")
-.then(Response => Response.json())
-.then(data => lol = data)
+// vv To test getExchange function vv
+// document.getElementById("lol").addEventListener("click", function(){
+//     getExchange(document.getElementById("input1").value, document.getElementById("input2").value)
+// })
+
+//Gets exchange rate of a specific currency based on base currency
+// async function getExchange(from,to){                                                           //Where "from" is base country's currency user would like to convert to another country's 
+//     let exchange = "";                                                                         // and "to" is the country's country the user would like to convert the base currency to
+//     await fetch ("https://api.ratesapi.io/api/latest?base=" + from + "&symbols=" + to)
+//     .then (Response => Response.json())
+//     .then (data => exchange = data["rates"][to])
+// }       
+
+
+
+// vv To test getAllExchange function vv
+// document.getElementById("lol").addEventListener("click", function(){
+//     getAllExchange(document.getElementById("input1").value)
+// })
+
+// vv Gets all exchange rates relative to given base currency vv
+// function getAllExchange(base){                                                           //Where base is the base currency that user wants to compare exchange rates to
+//     fetch ("https://api.ratesapi.io/api/latest?base="+base)
+//     .then (Response => Response.json())
+//     .then (data => console.log(data))
+// }
+
+
+
+// vv Gets exchange rate of a specific currency based on base currency from a specific date vv
+// function getPastExchange(yyyy, dd, mm, from, to){                                                           //Where "yyyy, dd, mm" represents the date the user would like to retrieve the exchange rates from
+//     exchange = "";
+//     await fetch ("https://api.ratesapi.io/api/" + yyyy + "-" + dd + "-" + mm + "?base=" + from + "&symbols=" + to)  //https://api.ratesapi.io/api/2010-01-12?base=USD&symbols=GBP <- Original link for ref.
+//     .then (Response => Response.json())
+//     .then (data => exchange = data["rates"][to])
+// }       
+
+
+// vv Gets all exchange rates relative to given base currency from a specific date vv
+// function getAllExchange(base){                                                            //Where base is the base currency that user wants to compare exchange rates to
+//     fetch ("https://api.ratesapi.io/api/" + yyyy + "-" + dd + "-" + mm + "base=" + base)  //https://api.ratesapi.io/api/2010-01-12?base=USD <- Original link for reference
+//     .then (Response => Response.json())
+//     .then (data => console.log(data))
+// }
